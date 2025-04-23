53e3dc9fe88d94c55b17c729a86386b2a7866b0e
Jamal Sikuku <sjamal1759@gmail.com>
2025-04-23 20:46:47 +0300

    Render home page from Storyblok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 // pages/_app.js
 
-import '../styles/globals.css';
-import '../lib/storyblok'; // this runs storyblokInit()
+import { useStoryblokState, storyblokInit, apiPlugin } from "@storyblok/react";
+import Page from "./components/Page";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+storyblokInit({
+  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_API_TOKEN,
+  use: [apiPlugin],
+  bridge: true,
+  components: {
+    page: Page
+  }
+});
+
+function App() {
+  const story = useStoryblokState({
+    story: {
+      name: 'Home',
+      content: { component: 'page' }
+    },
+    slug: "home",
+  });
+
+  return <Page blok={story?.content} />;
 }
-
-export default MyApp;
